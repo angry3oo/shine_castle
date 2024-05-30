e721bf3398e39f76e768bb77a6473263857ff9bf
hwansu <hwansu@localhost>
2024-05-31 00:06:06 +0900

    2024-05-31 김환수

---
+++ castle/src/main/java/com/shine/castle/config/CustomAuthenticationProvider.java
@@ -0,0 +1,20 @@
+package com.shine.castle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+
+@Configuration
+public class CustomAuthenticationProvider  {
+
+//throw new BadCredentialsException
+
+	
+	
+}
